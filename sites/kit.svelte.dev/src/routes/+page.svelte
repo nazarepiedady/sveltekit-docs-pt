62ed6f3756ba76c6f84cba0c0daf74861a230250
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-12-28 07:44:59 +0100

    docs(pt): update `kit.svelte.dev/routes/+page.svelte`

--- sites/kit.svelte.dev/src/routes/+page.svelte
+++ sites/kit.svelte.dev/src/routes/+page.svelte
@@ -92,7 +92,7 @@
 		}}
 	>
 		<span slot="license">
-			A SvelteKit é software<a target="_blank" rel="noreferrer" href="https://github.com/sveltejs/kit">livre e de código-aberto</a> lançada sob a licença do MIT.
+			A SvelteKit é software <a target="_blank" rel="noreferrer" href="https://github.com/sveltejs/kit">livre e de código-aberto</a> lançada sob a licença do MIT.
 		</span></Footer
 	>
 </div>
